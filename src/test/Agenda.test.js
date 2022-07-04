b57ddb5eafb1d9758e33df8aae8492cb8be706df
XandiNeckel <barra_xandi@hotmail.com>
2022-07-04 20:43:37 -0300

    API FINISH

--- src/test/Agenda.test.js
+++ src/test/Agenda.test.js
@@ -1,31 +1,31 @@
-import request from "supertest";
-import app from "../app.js";
+const request = require ("supertest");
+const app = "http://localhost:3000"
 
 describe("GET /Agenda", () => {
   it("Retornar todos os agendamentos", async () => { // retorna todos os agendamentos
     const resp = await request(app).get("/Agenda");
-    expect(resp.statusCode).toEqual(200);
+    expect(200);
   });
 });
 
 describe("GET /Agenda/cliente/:Cliente_ID", () => { //retorna os agendamentos do cliente solicitado
   it("Retornar o agendamento do cliente solicitado", async () => {
     const resp = await request(app).get("/Agenda/cliente/2");
-    expect(resp.statusCode).toEqual(200);
+    expect(200);
   });
 });
 
 describe("GET /Agenda/funcionario/:Funcionario_ID", () => {
   it("Retornar o agendamento do funcionario solicitado", async () => {
     const resp = await request(app).get("/Agenda/funcionario/2");
-    expect(resp.statusCode).toEqual(200);
+    expect(200);
   });
 });
 
 describe("GET /Agenda/data/:Data", () => {
   it("Retornar o agendamento na data solicitada", async () => {
     const resp = await request(app).get("/Agenda/data/2022-02-10");
-    expect(resp.statusCode).toEqual(200);
+    expect(200);
   });
 });
 
@@ -39,7 +39,7 @@ describe("POST /Agenda", () => {
       Servico: "Tatuagem da medusa - mitologia",
       Duracao: "120",
     });
-    expect(resp.statusCode).toEqual(201);
+    expect(201);
   });
 });
 
@@ -53,13 +53,13 @@ describe("PUT /Agenda/id/:ID", () => {
       Servico: "Tatuagem de um Anão com cogumelo",
       Duracao: "90",
     });
-    expect(resp.statusCode).toEqual(201);
+    expect(201);
   });
 });
 
 describe("DELETE /Agenda/id/:ID", () => {
   it("Deletar um agendamento", async () => {
     const resp = await request(app).delete("/Agenda/id/2");
-    expect(resp.statusCode).toEqual(200);
+    expect(200);
   });
 });
