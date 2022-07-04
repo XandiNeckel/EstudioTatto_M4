eadaf4a5230d64f3b1318f0480ea5ee160862905
XandiNeckel <barra_xandi@hotmail.com>
2022-07-04 16:27:05 -0300

    Finish API

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from "./src/app.js";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Servidor aberto na http://localhost:${port}`);
+  console.log(`Parabéns! Servidor aberto na http://localhost:${port}`);
 });
